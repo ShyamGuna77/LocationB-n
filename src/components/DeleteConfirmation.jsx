dbf341a49cc82c70bb0fa0200848aab4fe747353
Shyam <gshyamprasad2001@gmail.com>
2024-12-28 11:05:58 +0530

    Finished

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+
+const Timer = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(Timer);
+
+  // Decrease remainingTime every 10 ms
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => {
+        if (prevTime <= 0) {
+          clearInterval(interval); // Clear interval when time reaches 0
+          return 0;
+        }
+        return prevTime - 10;
+      });
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // Call onConfirm after 3000 ms
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onConfirm();
+    }, Timer);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +45,8 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      {/* Display progress bar */}
+      <progress value={Timer - remainingTime} max={Timer} />
     </div>
   );
 }
